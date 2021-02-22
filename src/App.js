03a079a01812a6fce775fb8c71d3414ff27f3a8c
Jolli <16289831+johannad8@users.noreply.github.com>
2021-02-22 23:29:48 +0100

    added new contents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import ScoreBoard from './layout/ScoreBoard';
+import GameBoard from './layout/GameBoard';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
+      <header className="app__header">
         <h1>TicTacToe</h1>
       </header>
+      <section className="app__container">
+        <ScoreBoard />
+        <GameBoard />
+      </section>
     </div>
   );
 }
