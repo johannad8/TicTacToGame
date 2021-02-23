cc631dec9692e8e0232476226730bbc2a7090c97
Jolli <16289831+johannad8@users.noreply.github.com>
2021-02-23 14:49:30 +0100

    added new functions for the game

--- src/layout/GameBoard.js
+++ src/layout/GameBoard.js
@@ -1,18 +1,77 @@
+import Tile from '../components/Tile';
+import Score from '../components/Score';
 import './_gameBoard.scss';
+import React, { useState } from "react";
 
 function GameBoard() {
+  const playerX = "X", playerO = "0";
+  const tileCount = 9;
+
+  const [winner, setWinner] = useState();
+  const [tiles, setTiles] = useState(Array(tileCount).fill(null));
+  const [clickCount, setClickCount] = useState(0);
+
+  function checkWinner(player) {
+    const possibleLines = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9],
+      [1,4,7],
+      [2,5,8],
+      [3,6,9],
+      [1,5,9],
+      [3,5,9]
+    ];
+
+    for (var i = 0; i < possibleLines.length; i++) {
+      const row = possibleLines[i];
+
+      if (tiles[row[0] -1] === player && tiles[row[1] -1] === player && tiles[row[2] -1] === player) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function resetGame() {
+    setTiles(Array(tileCount).fill(null));
+  }
+
+  function isGameOver() {
+    return !tiles.some((i) => i === undefined || i === null);
+  }
+
+  function tileClicked(e, index) {
+    e.preventDefault();
+    setClickCount(clickCount + 1)
+    tiles[index] = clickCount % 2 ? playerX : playerO;
+    setTiles(tiles);
+
+    if (checkWinner(tiles[index])) {
+      setWinner(tiles[index]);
+      resetGame();
+    }
+
+    if (isGameOver()) {
+      resetGame();
+    }
+  }
+
   return (
-    <section className="game-container">
-      <div className="tile">t1</div>
-      <div className="tile">t2</div>
-      <div className="tile">t3</div>
-      <div className="tile">t4</div>
-      <div className="tile">t5</div>
-      <div className="tile">t6</div>
-      <div className="tile">t7</div>
-      <div className="tile">t8</div>
-      <div className="tile">t9</div>
-    </section>
+    <>
+      <Score winner={winner} />
+      <section className="game-container">
+        <button value="Hello!" onClick={(e) => alert(e.target.value)}>
+          New Game
+        </button>
+        <div className="tiles-container">
+          {Array.from(Array(tileCount)).map((value, i) => {
+            return <Tile value={tiles[i]} tileClicked={tileClicked} key={i} index={i} />
+          })}
+        </div>
+      </section>
+    </>
   );
 }
 
