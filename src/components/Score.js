2073074a02ca83ff116f325e316fc914d5f04138
Jolli <16289831+johannad8@users.noreply.github.com>
2021-02-24 13:35:47 +0100

    removed old useffect and added new functions

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,37 +1,17 @@
 import './_score.scss';
-//import React, { Fragment } from "react";
-import React, { useState, useEffect } from "react";
 
 function Score(props) {
-  const [playerXScore, SetPlayerXScore] = useState(0);
-  const [player0Score, SetPlayer0Score] = useState(0);
-
-  useEffect(() => {
-    if (props.winner === "0") {
-      SetPlayer0Score(player0Score + 1);
-      console.log(0);
-    }
-    else if (props.winner === "X") {
-      SetPlayerXScore(playerXScore + 1);
-      console.log("X");
-    }
-  }, [props.winner]);
-
   return (
     <>
     <section className="score-container">
       <h2>Score</h2>
-      <div className="players">
-        <div className="X">
-          <span>X:</span>
-          <span className="player__score">{playerXScore}</span>
-        </div>
+      <div className="players player-X">
+        <h3>Player X</h3>
+        <p className="players__score">{props.playerX}</p>
       </div>
-      <div className="players">
-        <div className="0">
-          <span>0:</span>
-          <span className="player__score">{player0Score}</span>
-        </div>
+      <div className="players player-O">
+        <h3>Player O</h3>
+        <p className="players__score">{props.player0}</p>
       </div>
     </section>
     </>
